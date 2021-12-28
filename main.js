5831f89a16aa0881a34a10afb3c2600ec139251f
21ciprian <2021ciprianoffice@gmail.com>
2021-12-28 18:15:01 +0000

    Added collision detection to the ball

--- main.js
+++ main.js
@@ -3,11 +3,12 @@ const canvas = document.querySelector('#breakout')
 const ctx = canvas.getContext('2d')
 let x = canvas.width / 2
 let y = canvas.height - 30
-let dx = 2
-let dy = -2
+let dx = 1
+let dy = -1
+let ballRadius = 5
 function drawBall() {
   ctx.beginPath()
-  ctx.arc(x, y, 5, 0, Math.PI * 2)
+  ctx.arc(x, y, ballRadius, 0, Math.PI * 2)
   ctx.fillStyle = 'white'
   ctx.fill()
   ctx.closePath()
@@ -16,8 +17,15 @@ function drawBall() {
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height)
   drawBall()
+  if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+    dx = -dx;
+  }
+  if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
+    dy = -dy;
+  }
   x += dx
   y += dy
 }
+
 
 setInterval(draw, 10)
